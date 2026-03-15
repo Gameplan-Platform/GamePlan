998522a237e2c4a2836f5b5999344526891e59b5
m-marcarelli <marissamarcarelli@gmail.com>
2026-03-15 00:17:45 -0700

    added logo refs #gp-9

--- packages/frontend/src/pages/TitleScreen.tsx
+++ packages/frontend/src/pages/TitleScreen.tsx
@@ -1,7 +1,9 @@
+import logo from '../assets/logo.png'
+
 export default function TitleScreen() {
-    return (
-      <div className = "flex items-center justify-center min-h-screen bg-white">
-        <h1 className = "text-2xl font-bold text-purple-700">GamePlan</h1>
-      </div>
-    )
-  }
+  return (
+    <div className = "flex flex-col items-center justify-center min-h-screen bg-white">
+      <img src = {logo} alt = "GamePlan logo" className = "w-48" />
+    </div>
+  )
+}
