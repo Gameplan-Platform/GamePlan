92b80bc30a43a2bd80e6a2116976289cca880ad1
m-marcarelli <marissamarcarelli@gmail.com>
2026-03-15 00:10:23 -0700

    title screen refs #cp-9

---
+++ packages/frontend/src/pages/TitleScreen.tsx
@@ -0,0 +1,7 @@
+export default function TitleScreen() {
+    return (
+      <div className = "flex items-center justify-center min-h-screen bg-white">
+        <h1 className = "text-2xl font-bold text-purple-700">GamePlan</h1>
+      </div>
+    )
+  }
